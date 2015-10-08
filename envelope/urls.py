d247c10978e20988efc64df67b4e1fd0c095b312
Ahmed Salman <ahmed.salman.noor@gmail.com>
2015-10-08 15:19:54 +0400

    move the detail view to solution, company and products

--- envelope/urls.py
+++ envelope/urls.py
@@ -11,13 +11,6 @@ from envelope.views import ContactView
 
 urlpatterns = patterns('',
     url(r'^$', ContactView.as_view(), name='envelope-contact'),
-    # url(r'^(?P<company_slug>[a-zA-Z0-9\-]+)/$', CompanyContactList.as_view(), name='company_contact'),
-    # url(r'^(?P<company_slug>[a-zA-Z0-9\-]+)/company/contact/$', CompanyContactAdd.as_view(), name='company_contact'),
-    # url(r'^(?P<product_slug>[a-zA-Z0-9\-]+)/product/contact/$', ProductContactAdd.as_view(), name='product_contact'),
-    # # url(r'^(?P<solution_slug>[a-zA-Z0-9\-]+)/solution/contact/$', SolutionContactAdd.as_view(), name='solution_contact'),
-    # url(r'^(?P<company_slug>[a-zA-Z0-9\-]+)/(?P<email_type>[a-zA-Z0-9\-]+)/$', CompanyContactList.as_view(), name='company_contact'),
     # url(r'^detail/(?P<company_slug>[a-zA-Z0-9\-]+)-(?P<email_id>[a-zA-Z0-9\-]+)$', CompanyContactDetail.as_view(), name='company_contact_detail'),
 
-
-    # url(r'^(?P<solutions_slug>[a-zA-Z0-9\-]+)/add/$', SolutionContactAdd.as_view(), name='solution_contact'),
 )
--- envelope/views.py
+++ envelope/views.py
@@ -7,30 +7,15 @@ Views used to process the contact form.
 """
 
 import logging
-from django.core.urlresolvers import reverse
 from django.contrib import messages
-from django.contrib.auth.decorators import user_passes_test
 from django.http import HttpResponseBadRequest
-from django.http import HttpResponseRedirect
 from django.shortcuts import redirect
-from django.utils.decorators import method_decorator
 from django.views.generic import FormView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from django.views.generic import ListView, DetailView
-from django.utils import timezone
+from django.views.generic.edit import CreateView
 from django.utils.translation import ugettext_lazy as _
 
-# from allauth.account.decorators import verified_email_required
-# from articles.utils import slugify_unique, check_active
-# from companies.views import get_company
-# from products.views import get_product
-# from solutions.views import get_solution
 from envelope import signals
-from envelope.settings import DEFAULT_CONTACT_CHOICES, CONTACT_CHOICES
 from envelope.forms import ContactForm
-# from envelope.models import CompanyContact, ProductContact, SolutionContact
-from general.tasks import task_send_email
-
 
 logger = logging.getLogger('envelope.views')
 
@@ -186,71 +171,6 @@ class BaseContact(CreateView):
             **response_kwargs
         )
 
-
-# #Todo can be converted to all the contact list by replacing "CompanyContact" model with ContactBase Model
-# class CompanyContactList(ListView):
-#
-#     allow_empty = True
-#     paginate_by = 10
-#     template_name = 'envelope/listing.html'
-#     model = CompanyContact
-#     company = None
-#     email_type = None
-#
-#     def get_queryset(self):
-#         queryset = super(CompanyContactList, self).get_queryset()
-#         queryset = queryset.filter(company=self.company).order_by('-created')
-#         if self.email_type == '-1':                                   #Deleted emails
-#             queryset = queryset.filter(company=self.company, state=-1)
-#         elif self.email_type == '1':                                  #Replied emails
-#             queryset = queryset.filter(company=self.company, state=1)
-#         elif self.email_type == '2':                                  #Pending reply emails
-#             queryset = queryset.filter(company=self.company, state=2)
-#         return queryset
-#
-#
-#     def get(self, request, *args, **kwargs):
-#         try:
-#             self.email_type = kwargs['email_type']
-#         except KeyError:
-#             self.email_type = '0'
-#         slug = kwargs['company_slug']
-#         self.user = request.user
-#         self.company, allow_edit = get_company(slug, self.user)
-#         return super(CompanyContactList, self).get(request, *args, **kwargs)
-#
-#     def get_context_data(self, **kwargs):
-#         context = super(CompanyContactList, self).get_context_data(**kwargs)
-#         context['now'] = timezone.now()
-#         context['company'] = self.company
-#         context['email_type'] = self.email_type
-#         return context
-#
-#
-# class CompanyContactDetail(DetailView):
-#
-#     template_name = 'envelope/detail.html'
-#     model = CompanyContact
-#     company = None
-#     object = None
-#
-#
-#     def get(self, request, *args, **kwargs):
-#         email_id = kwargs['email_id']
-#         slug = kwargs['company_slug']
-#         self.user = request.user
-#         self.company, allow_edit = get_company(slug, self.user)
-#         self.object = CompanyContact.objects.get(id=email_id)
-#         return super(CompanyContactList, self).get(request, *args, **kwargs)
-#
-#
-#     def get_context_data(self, **kwargs):
-#         context = super(CompanyContactDetail, self).get_context_data(**kwargs)
-#         context['now'] = timezone.now()
-#         context['company'] = self.company
-#         context['item'] = self.object
-#         return context
-#
 
 # def filter_spam(sender, request, form, **kwargs):
 #     """
